f8125e014b4c5acfe459ea9124f0b4632cc0bfe1
Alphonce <dulkochieng@gmail.com>
2023-11-10 15:18:17 +0300

    update test case

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 from unittest import main
 from tests.test_base_model import TestBase
+from tests.test_amenity import TestAmenity
+from tests.test_city import TestCity
+from tests.test_user import TestUser
+from tests.test_review import TestReview
+from tests.test_state import TestState
+
 
 
 if __name__ == "__main__":
