b256bae602840a5142ec3cbd7ba326ab6f2c5498
Alphonce <dulkochieng@gmail.com>
2023-11-10 13:38:29 +0300

    update tests

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,20 +7,22 @@ import re
 
 class TestBase(TestCase):
     def setUp(self):
-        self.mockedBase = Mock(spec=BaseModel)
-        self.eyed = "ad8bb9c6-ff2b-4884-bd86-68e854b18fac"
-        time = datetime(2023, 11, 10, 12, 21, 22, 246397)
-        self.updatetime = datetime(2024, 1, 10, 12, 21, 22, 246397)
-        self.mockedBase.return_value = BaseModel(id=self.eyed,created_at=time, updated_at=time)
-        self.baseone = self.mockedBase()
+        self.baseone = BaseModel()
+        self.basetwo = BaseModel()
 
     def tearDown(self):
         del self.baseone
+        del self.basetwo
 
     def test_init(self):
-        self.assertEqual(self.baseone.id, self.eyed)
+        self.assertIsInstance(self.baseone.id, str)
+        self.assertEqual(len(self.baseone.id), 36)
         self.assertIsInstance(self.baseone.created_at, datetime)
         self.assertIsInstance(self.baseone.updated_at, datetime)
+        self.assertIsInstance(self.basetwo.id, str)
+        self.assertEqual(len(self.basetwo.id), 36)
+        self.assertIsInstance(self.basetwo.created_at, datetime)
+        self.assertIsInstance(self.basetwo.updated_at, datetime)
 
     def test_save(self):
         self.baseone.save()
